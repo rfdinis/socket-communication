4caf0f78f4c2ff915bfbb64ea8b38c0c0dcf4d7d
Ricardo Dinis <rffdinis@gmail.com>
2018-12-31 23:10:14 +0000

    header file

--- socklib/Include/socklib.h
+++ socklib/Include/socklib.h
@@ -11,5 +11,29 @@
 typedef int     sfd_t;              /* socket type */
 typedef struct  sockaddr_in addr_t; /* address type */
 
+/********************General Purppose**************************/
+sfd_t sock_tcp(void);
+sfd_t sock_udp(void);
+addr_t sock_addr(const char* ip, int port);
+int sock_reuse(sfd_t s);
+int sock_shut(sfd_t s);
+
+/*************************Unicast******************************/
+int sock_send(sfd_t s, uint8_t* bytes, int size);
+int sock_receive(sfd_t s, uint8_t* bytes, int size);
+
+/*Passive*/
+int sock_bind(sfd_t s, addr_t addr);
+int sock_listen(sfd_t s, int len);
+sfd_t sock_accept(sfd_t s, addr_t addr);
+
+/*Active*/
+sfd_t sock_connect(sfd_t s, addr_t addr);
+
+/************************Multicast*****************************/
+int sock_multicast(sfd_t s, const char* ipgroup);
+int sock_receivefrom(sfd_t s, uint8_t* buffer, int size, addr_t addr);
+int sock_sendto(sfd_t s, uint8_t* bytes, int size, addr_t addr);
+
 
 #endif /*__SOCK_LIB__*/
